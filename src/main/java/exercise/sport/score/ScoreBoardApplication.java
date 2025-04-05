d58593013e10d2375fc97a587c08a0120c5bdf25
Peace <55354072+nickimilie@users.noreply.github.com>
2025-04-05 19:58:27 +0200

    test application

--- src/main/java/exercise/sport/score/ScoreBoardApplication.java
+++ src/main/java/exercise/sport/score/ScoreBoardApplication.java
@@ -1,10 +1,25 @@
 package exercise.sport.score;
 
-/**
- * Hello world!
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ScoreBoardApplication {
+
+    static String END_COMMAND = "exit";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        startApplication();
+    }
+
+    public static List<String> startApplication() {
+        List<String> commands = new ArrayList<>();
+        String command = "start";
+        Scanner scanner = new Scanner(System.in);
+        while(!command.equals(END_COMMAND)) {
+            command = scanner.nextLine();
+            commands.add(command);
+        }
+        return commands;
     }
 }
--- src/test/java/exercise/sport/score/ScoreBoardApplicationTest.java
+++ src/test/java/exercise/sport/score/ScoreBoardApplicationTest.java
@@ -1,19 +1,40 @@
 package exercise.sport.score;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
 public class ScoreBoardApplicationTest {
 
-    /**
-     * Rigorous Test :-)
-     */
+    static String END_COMMAND = "exit";
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    void should_end_program_with_end_command() {
+        String[] inputLines = new String[]{
+                "First command",
+                "Second Command",
+                END_COMMAND,
+                "Additional command"
+        };
+
+        String[] expectedLines = Arrays.copyOf(inputLines, inputLines.length - 1);
+        List<String> expectedLinesList = Arrays.stream(expectedLines).toList();
+
+        InputStream inputStream = System.in;
+
+        try{
+            System.setIn(new ByteArrayInputStream(String.join("\n", inputLines).getBytes()));
+            List<String> actualLinesList = ScoreBoardApplication.startApplication();
+            assertThat(actualLinesList).isEqualTo(expectedLinesList);
+        }
+        finally {
+            System.setIn(inputStream);
+        }
+
     }
 }
