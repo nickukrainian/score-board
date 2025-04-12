f594b1450097ccb9bfd6cce1b5054bf7e7c137ad
Peace <55354072+nickimilie@users.noreply.github.com>
2025-04-12 15:49:26 +0200

    pattern

--- src/main/java/exercise/sport/score/service/VerificationService.java
+++ src/main/java/exercise/sport/score/service/VerificationService.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class VerificationService {
 
     public void verifyScore(String score) {
-        Pattern pattern = Pattern.compile("\\d{3}?");
+        Pattern pattern = Pattern.compile("\\d{1,3}");
         if (!pattern.matcher(score).matches()) throw new IllegalArgumentException();
     }
 
